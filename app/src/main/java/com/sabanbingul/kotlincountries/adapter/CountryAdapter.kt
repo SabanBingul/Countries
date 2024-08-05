2d5baff0c3fd214d9bf6b505c308d43b57cf0c3a
Şaban Bingül <sabanbngl00@gmail.com>
2024-08-05 19:11:23 +0300

    RecyclerViewAdapter completed

--- app/src/main/java/com/sabanbingul/kotlincountries/adapter/CountryAdapter.kt
+++ app/src/main/java/com/sabanbingul/kotlincountries/adapter/CountryAdapter.kt
@@ -1,4 +1,43 @@
 package com.sabanbingul.kotlincountries.adapter
 
-class CountryAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.sabanbingul.kotlincountries.R
+import com.sabanbingul.kotlincountries.model.Country
+
+class CountryAdapter(val countryList: ArrayList<Country>): RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+
+
+    class CountryViewHolder(var view: View) : RecyclerView.ViewHolder(view){
+        val name: TextView = view.findViewById(R.id.name)
+        val region: TextView = view.findViewById(R.id.region)
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_country, parent, false)
+        return CountryViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return countryList.size
+    }
+
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+        holder.name.text = countryList[position].countryName
+        holder.region.text = countryList[position].countryRegion
+
+    }
+
+    fun updateCountryList(newCountryList : List<Country>){
+        countryList.clear()
+        countryList.addAll(newCountryList)
+        notifyDataSetChanged()
+    }
+
 }
