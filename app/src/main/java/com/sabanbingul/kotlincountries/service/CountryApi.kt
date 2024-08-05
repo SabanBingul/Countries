b4e0c09d7f650157050d261c349fc88c17f17b61
Şaban Bingül <sabanbngl00@gmail.com>
2024-08-06 00:50:56 +0300

    Service classes added and got data with retrofit and rxjava. Model class revised. FeedViewModel class revised with retrofit and rxjava

---
+++ app/src/main/java/com/sabanbingul/kotlincountries/service/CountryApi.kt
@@ -0,0 +1,13 @@
+package com.sabanbingul.kotlincountries.service
+
+import com.sabanbingul.kotlincountries.model.Country
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface CountryApi {
+ //https://raw.githubusercontent.com/atilsamancioglu/IA19-DataSetCountries/master/countrydataset.json
+    @GET("atilsamancioglu/IA19-DataSetCountries/master/countrydataset.json")
+    fun getCountries(): Single<List<Country>>
+
+
+}
